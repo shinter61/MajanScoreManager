2c91cb10818f755b8f8adcd44aa93643301983e6
shinter61 <shinter61@gmail.com>
2021-03-23 17:38:54 +0900

    Initial Commit

---
+++ MajanScoreManager/MajanScoreManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MajanScoreManagerApp.swift
+//  MajanScoreManager
+//
+//  Created by 松本真太朗 on 2021/03/23.
+//
+
+import SwiftUI
+
+@main
+struct MajanScoreManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
