c33c8565e2fcd2b0b2e2f5905bc1d8099d19b92c
shinter61 <shinter61@gmail.com>
2021-03-24 01:08:22 +0900

    Add game starter screen

--- MajanScoreManager/Views/ContentView.swift
+++ MajanScoreManager/Views/ContentView.swift
@@ -10,47 +10,7 @@ import SwiftUI
 struct ContentView: View {
     @EnvironmentObject var modelData: ModelData
     var body: some View {
-        let player1: Player = modelData.gameData.players[0]
-        let player2: Player = modelData.gameData.players[1]
-        let player3: Player = modelData.gameData.players[2]
-        let player4: Player = modelData.gameData.players[3]
-        ZStack {
-            EndGameButton()
-            GeometryReader { geometry in
-                if player1.isRiichi {
-                    RiichiBar()
-                        .position(x: 0.5 * geometry.size.width, y: 0.39 * geometry.size.height)
-                }
-                PlayerScore(player: player1)
-                    .rotationEffect(Angle(degrees: 180))
-                    .position(x: 0.5 * geometry.size.width, y: 0.30 * geometry.size.height)
-                if player2.isRiichi {
-                    RiichiBar()
-                        .rotationEffect(Angle(degrees: 90))
-                        .position(x: 0.67 * geometry.size.width, y: 0.5 * geometry.size.height)
-                }
-                PlayerScore(player: player2)
-                    .rotationEffect(Angle(degrees: -90))
-                    .position(x: 0.81 * geometry.size.width, y: 0.5 * geometry.size.height)
-                if player3.isRiichi {
-                    RiichiBar()
-                        .rotationEffect(Angle(degrees: 0))
-                        .position(x: 0.5 * geometry.size.width, y: 0.61 * geometry.size.height)
-                }
-                PlayerScore(player: player3)
-                    .rotationEffect(Angle(degrees: 0))
-                    .position(x: 0.5 * geometry.size.width, y: 0.70 * geometry.size.height)
-                if player4.isRiichi {
-                    RiichiBar()
-                        .rotationEffect(Angle(degrees: 90))
-                        .position(x: 0.33 * geometry.size.width, y: 0.5 * geometry.size.height)
-                }
-                PlayerScore(player: player4)
-                    .rotationEffect(Angle(degrees: 90))
-                    .position(x: 0.19 * geometry.size.width, y: 0.5 * geometry.size.height)
-            }
-        }
-        .background(Color.green)
+        StartGame()
     }
 }
 
---
+++ MajanScoreManager/Views/StartGame.swift
@@ -0,0 +1,63 @@
+//
+//  StartGame.swift
+//  MajanScoreManager
+//
+//  Created by 松本真太朗 on 2021/03/23.
+//
+
+import SwiftUI
+
+struct StartGame: View {
+    @EnvironmentObject var modelData: ModelData
+    @State private var name1: String = ""
+    @State private var name2: String = ""
+    @State private var name3: String = ""
+    @State private var name4: String = ""
+    var body: some View {
+        NavigationView {
+            VStack {
+                Group {
+                    TextField("Player 1", text: $name1, onCommit: {
+                        modelData.gameData.players[0].name = name1
+                    })
+                    TextField("Player 2", text: $name2, onCommit: {
+                        modelData.gameData.players[1].name = name2
+                    })
+                    TextField("Player 3", text: $name3, onCommit: {
+                        modelData.gameData.players[2].name = name3
+                    })
+                    TextField("Player 4", text: $name4, onCommit: {
+                        modelData.gameData.players[3].name = name4
+                    })
+                }
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                .frame(width: 200, height: 50, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+
+                let isEmpty: Bool = name1.isEmpty || name2.isEmpty || name3.isEmpty || name4.isEmpty
+                Button(action: {
+                }) {
+                    NavigationLink(destination: Game()) {
+                        ZStack {
+                            RoundedRectangle(cornerRadius: /*@START_MENU_TOKEN@*/25.0/*@END_MENU_TOKEN@*/)
+                                .fill(Color.yellow)
+                                .frame(width: 180, height: 50)
+                            Text("対局開始")
+                                .foregroundColor(.black)
+                        }
+                    }
+                    .disabled(isEmpty)
+                    .navigationTitle("MajanScoreManager")
+                }
+                .padding(.top, 50)
+            }
+        }
+        .padding(0.0)
+    }
+}
+
+struct StartGame_Previews: PreviewProvider {
+    static var previews: some View {
+        StartGame()
+            .environmentObject(ModelData())
+    }
+}
