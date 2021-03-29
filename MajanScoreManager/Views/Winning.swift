4758aa89fad293c4129b151e0cdc8cd50ac312b9
studik-s <float.kuina@icloud.com>
2021-03-29 18:19:43 +0900

    多分いけました

--- MajanScoreManager/Views/Winning.swift
+++ MajanScoreManager/Views/Winning.swift
@@ -120,9 +120,15 @@ struct Winning: View {
             }
         }
     }
+    //pointsのindex取得
+    func getPointsIndex(PointID: Int) -> Int {
+        return modelData.points.firstIndex(of: modelData.PointsDRs[type.rawValue - 1].pointsDoubles[doubleID].points[PointID])!
+    }
     
     func winningProcess() -> Void {
         showingAlert = inputValidate(type: type,  winnerID: winnerID, loserID: loserID, doubleID: doubleID, pointID: pointID)
+        //PointID変換
+        pointID = getPointsIndex(PointID: pointID)
         if showingAlert {
             AlertText = "未入力の項目があります"
             return
