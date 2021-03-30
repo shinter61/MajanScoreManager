085b7597abbeb8bd6546e5c9b5d3a7d4bdf35b76
shinter61 <shinter61@gmail.com>
2021-03-30 19:46:17 +0900

    Style start game

--- MajanScoreManager/Views/StartGame.swift
+++ MajanScoreManager/Views/StartGame.swift
@@ -16,49 +16,82 @@ struct StartGame: View {
     @State private var navigateGameMenu: Bool = false
     var body: some View {
         NavigationView {
-            VStack {
-                Group {
-                    TextField("Player 1", text: $name1, onCommit: {
-                        modelData.gameData.players[0].name = name1
-                    })
-                    TextField("Player 2", text: $name2, onCommit: {
-                        modelData.gameData.players[1].name = name2
-                    })
-                    TextField("Player 3", text: $name3, onCommit: {
-                        modelData.gameData.players[2].name = name3
-                    })
-                    TextField("Player 4", text: $name4, onCommit: {
-                        modelData.gameData.players[3].name = name4
-                    })
-                }
-                .textFieldStyle(RoundedBorderTextFieldStyle())
-                .frame(width: 200, height: 50, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
-
-                Button(action: {
-                    modelData.gameData.players[0].name = name1.isEmpty ? "Player1" : name1
-                    modelData.gameData.players[1].name = name2.isEmpty ? "Player2" : name2
-                    modelData.gameData.players[2].name = name3.isEmpty ? "Player3" : name3
-                    modelData.gameData.players[3].name = name4.isEmpty ? "Player4" : name4
-                    navigateGameMenu = true
-                }) {
-                        ZStack {
-                            RoundedRectangle(cornerRadius: /*@START_MENU_TOKEN@*/25.0/*@END_MENU_TOKEN@*/)
-                                .fill(Color.yellow)
-                                .frame(width: 180, height: 50)
-                            Text("対局開始")
-                                .foregroundColor(.black)
+            ZStack {
+                self.backGroundColor().edgesIgnoringSafeArea(.all)
+                VStack {
+                    GeometryReader { geometry in
+                        Text("麻雀点数管理")
+                            .font(.custom("Shippori Mincho", size: 60))
+                            .fontWeight(.bold)
+                            .foregroundColor(Color.white)
+                            .position(x: geometry.size.width * 0.5, y: geometry.size.height * 0.02)
+                        Text("プレイヤー名を入力してください")
+                            .font(.custom("Shippori Mincho", size: 20))
+                            .fontWeight(.regular)
+                            .position(x: geometry.size.width * 0.5, y: geometry.size.height * 0.2)
+                        Group {
+                            TextField("Player 1", text: $name1, onCommit: {
+                                modelData.gameData.players[0].name = name1
+                            })
+                            .position(x: geometry.size.width * 0.5, y: geometry.size.height * 0.3)
+                            TextField("Player 2", text: $name2, onCommit: {
+                                modelData.gameData.players[1].name = name2
+                            })
+                            .position(x: geometry.size.width * 0.5, y: geometry.size.height * 0.4)
+                            TextField("Player 3", text: $name3, onCommit: {
+                                modelData.gameData.players[2].name = name3
+                            })
+                            .position(x: geometry.size.width * 0.5, y: geometry.size.height * 0.5)
+                            TextField("Player 4", text: $name4, onCommit: {
+                                modelData.gameData.players[3].name = name4
+                            })
+                            .position(x: geometry.size.width * 0.5, y: geometry.size.height * 0.6)
                         }
+                        .font(.system(size: 24))
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                        .frame(width: geometry.size.width * 0.75, alignment: .center)
+
+                        Button(action: {
+                            modelData.gameData.players[0].name = name1.isEmpty ? "Player1" : name1
+                            modelData.gameData.players[1].name = name2.isEmpty ? "Player2" : name2
+                            modelData.gameData.players[2].name = name3.isEmpty ? "Player3" : name3
+                            modelData.gameData.players[3].name = name4.isEmpty ? "Player4" : name4
+                            navigateGameMenu = true
+                        }) {
+                                ZStack {
+                                    RoundedRectangle(cornerRadius: /*@START_MENU_TOKEN@*/25.0/*@END_MENU_TOKEN@*/)
+                                        .fill(Color.yellow)
+                                        .frame(width: 180, height: 60)
+                                    Text("対局開始")
+                                        .foregroundColor(.white)
+                                        .font(.custom("Shippori Mincho", size: 24))
+                                        .fontWeight(.bold)
+                                }
+                        }
+                        .position(x: geometry.size.width * 0.5, y: geometry.size.height * 0.7)
+                        .padding(.top, 50)
+                        .navigationTitle("")
+                    
+                        NavigationLink(destination: Game(rootIsActive: self.$navigateGameMenu).navigationBarHidden(true), isActive: self.$navigateGameMenu) {
+                            EmptyView()
+                        }
+                        .isDetailLink(false)
+                    }
                 }
-                .padding(.top, 50)
-                .navigationTitle("MahjongScoreManager")
-            
-                NavigationLink(destination: Game(rootIsActive: self.$navigateGameMenu).navigationBarHidden(true), isActive: self.$navigateGameMenu) {
-                    EmptyView()
-                }
-                .isDetailLink(false)
             }
         }
-        .padding(0.0)
+    }
+    
+    private func backGroundColor() -> LinearGradient {
+        // 左上から右下にポイントを設定する。
+        let start = UnitPoint.init(x: 0, y: 0) // 左上(始点)
+        let end = UnitPoint.init(x: 1, y: 1) // 右下(終点)
+
+        let colors = Gradient(colors: [Color(red: 102 / 255, green: 255 / 255, blue: 255 / 255), Color(red: 0 / 255, green: 211 / 255, blue: 130 / 255)])
+
+        let gradientColor = LinearGradient(gradient: colors, startPoint: start, endPoint: end)
+
+        return gradientColor
     }
 }
 
