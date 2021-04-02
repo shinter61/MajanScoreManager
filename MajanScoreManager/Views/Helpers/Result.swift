ff118c5bf212b16c15816a6363d04b8277ddc417
shinter61 <shinter61@gmail.com>
2021-04-03 03:26:25 +0900

    Style result view

--- MajanScoreManager/Views/Helpers/Result.swift
+++ MajanScoreManager/Views/Helpers/Result.swift
@@ -11,89 +11,75 @@ struct Result: View {
     @EnvironmentObject var modelData: ModelData
     var i: Int //ResultGameDatas's index
     var body: some View {
-        let gameCount = modelData.getGameCount()
-        if gameCount != 0 {
-            let place = modelData.getPlace(index: i)
-            let bets = modelData.getBets(index: i)
-            let endType = modelData.getEndType(index: i)
-            let waitersCount = modelData.getWaitersCount(index: i)
-            let winnersCount = modelData.getWinnersCount(index: i)
-            HStack {
-                VStack {
-                    Text(String(place[0]))
-                    Text(String(place[1]))
-                    Text(String(bets))
-                }
-                .padding()
-    
-                VStack(spacing: 10) {
-                    Text(String(endType))
-                    if endType == "流局" {
-                        HStack{
-                           Text("聴牌者: ")
-                            if waitersCount == 0 {
-                                Text("なし")
-                            } else {
-                                ForEach(0..<waitersCount) { index in
-                                    Text(String(modelData.resultGameDatas[i].waitersR[index]))
-                                }
+        let place = modelData.getPlace(index: i)
+        let bets = modelData.getBets(index: i)
+        let endType = modelData.getEndType(index: i)
+        let waitersCount = modelData.getWaitersCount(index: i)
+        let winnersCount = modelData.getWinnersCount(index: i)
+        HStack {
+            VStack {
+                StyledText(text: String(place[0]), size: 16)
+                StyledText(text: String(place[1]), size: 16)
+                StyledText(text: String(bets), size: 16)
+            }
+            .padding()
+
+            VStack(spacing: 10) {
+                StyledText(text: String(endType), size: 16)
+                if endType == "流局" {
+                    HStack{
+                        StyledText(text: "聴牌者: ", size: 16)
+                        if waitersCount == 0 {
+                            StyledText(text: "なし", size: 16)
+                        } else {
+                            ForEach(0..<waitersCount) { index in
+                                StyledText(text: String(modelData.resultGameDatas[i].waitersR[index]), size: 16)
                             }
+                        }
+                        Spacer()
+                    }
+                } else if endType == "自摸" {
+                    Group {
+                        HStack{
+                            StyledText(text: "和了者: \(modelData.getWinnersName(index: i, winIndex: 0))", size: 16)
                             Spacer()
                         }
-                    } else if endType == "自摸" {
-                        Group {
-                            HStack{
-                                Text("和了者: ")
-                                Text(String(modelData.getWinnersName(index: i, winIndex: 0)))
-                                Spacer()
-                            }
-                            HStack{
-                                Text("飜/点数: ")
-                                Text(modelData.getWinnersDouble(index: i, winIndex: 0))
-                                Text("/")
-                                Text(modelData.getWinnersScore(index: i, winIndex: 0))
-                                Spacer()
-                            }
-                        }
-                    } else {
-                        ForEach(0..<winnersCount) { winIndex in
-                            Group {
-                                HStack{
-                                    Text("和了者: ")
-                                    Text(String(modelData.getWinnersName(index: i, winIndex: winIndex)))
-                                    Spacer()
-                                }
-                                HStack{
-                                    Text("飜/点数: ")
-                                    Text(modelData.getWinnersDouble(index: i, winIndex: winIndex))
-                                    Text("/")
-                                    Text(modelData.getWinnersScore(index: i, winIndex: winIndex))
-                                    Spacer()
-                                }
-                            }
-                            if winIndex != (winnersCount - 1) {
-                                Divider()
-                            } else {
-                                Spacer(minLength: 5)
-                            }
-                        }
                         HStack{
-                            Text("放銃者: \(modelData.getLoserName(index: i))")
+                            StyledText(text: "飜/点数:", size: 16)
+                            StyledText(text: " \(modelData.getWinnersDouble(index: i, winIndex: 0)) / \(modelData.getWinnersScore(index: i, winIndex: 0))", size: 16)
                             Spacer()
                         }
                     }
-                } //End VStack
-                .frame(maxWidth: .infinity)
-                .padding(.leading, 20)
-                Divider()
-
-             } //End HStack
-
-         } //End if
-         
+                } else {
+                    ForEach(0..<winnersCount) { winIndex in
+                        Group {
+                            HStack{
+                                StyledText(text: "和了者: \(modelData.getWinnersName(index: i, winIndex: 0))", size: 16)
+                                Spacer()
+                            }
+                            HStack{
+                                StyledText(text: "飜/点数:", size: 16)
+                                StyledText(text: " \(modelData.getWinnersDouble(index: i, winIndex: winIndex)) / \(modelData.getWinnersScore(index: i, winIndex: winIndex))", size: 16)
+                                Spacer()
+                            }
+                        }
+                        if winIndex != (winnersCount - 1) {
+                            Divider()
+                        } else {
+                            Spacer(minLength: 5)
+                        }
+                    }
+                    HStack{
+                        StyledText(text: "放銃者: \(modelData.getLoserName(index: i))", size: 16)
+                        Spacer()
+                    }
+                }
+            } //End VStack
+            .frame(maxWidth: .infinity)
+         } //End HStack
     } //End body
-    
 } //End Result
+
 struct Result_Previews: PreviewProvider {
     static var previews: some View {
         Result(i: 0)
--- MajanScoreManager/Views/Results.swift
+++ MajanScoreManager/Views/Results.swift
@@ -13,31 +13,39 @@ struct Results: View {
 
     var body: some View {
         let gameCount = modelData.getGameCount()
-        GeometryReader { geometry in
-            VStack {
-                Text("戦績一覧")
-                    .position(x: geometry.size.width * 0.5, y: geometry.size.height * 0.05)
-                    .frame(height: geometry.size.height / 10)
-                if gameCount != 0 {
-                    List {
-                        ForEach(0..<gameCount) { index in
-                            Result(i: index)
+        ZStack {
+            Color(red: 238 / 255, green: 238 / 255, blue: 238 / 255).edgesIgnoringSafeArea(.all)
+            
+            GeometryReader { geometry in
+                VStack {
+                    Text("戦績一覧")
+                        .font(.custom("Shippori Mincho", size: 20))
+                        .fontWeight(.regular)
+                        .foregroundColor(Color(red: 58 / 255, green: 76 / 255, blue: 99 / 255))
+                        .position(x: geometry.size.width * 0.5, y: geometry.size.height * 0.05)
+                        .frame(height: geometry.size.height / 10)
+                    if gameCount != 0 {
+                        List {
+                            ForEach(0..<gameCount) { index in
+                                Result(i: index)
+                            }
+                            .listRowBackground(Color(red: 238 / 255, green: 238 / 255, blue: 238 / 255))
                         }
+                    } else {
+                        Spacer()
                     }
-                } else {
-                    Spacer()
+                    backResultButton(showingResultView: self.$showingResultView)
+                        .position(x: geometry.size.width * 0.5, y: geometry.size.height * 0.05)
+                        .frame(height: geometry.size.height / 10)
                 }
-                backResultButton(showingResultView: self.$showingResultView)
-                    .position(x: geometry.size.width * 0.5, y: geometry.size.height * 0.05)
-                    .frame(height: geometry.size.height / 10)
             }
         }
     }
 }
 
 struct Results_Previews: PreviewProvider {
- static var previews: some View {
-     Results(showingResultView: .constant(false))
-         .environmentObject(ModelData())
- }
+    static var previews: some View {
+        Results(showingResultView: .constant(false))
+            .environmentObject(ModelData())
+    }
 }
