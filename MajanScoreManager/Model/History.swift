248718d39f173768b053acc7393597f41d216b9f
shinter61 <shinter61@gmail.com>
2021-04-06 02:37:07 +0900

    Create history model

---
+++ MajanScoreManager/Model/History.swift
@@ -0,0 +1,20 @@
+//
+//  History.swift
+//  MajanScoreManager
+//
+//  Created by 松本真太朗 on 2021/04/06.
+//
+
+import Foundation
+
+struct History: Hashable, Codable, Identifiable {
+    var id: UUID = UUID()
+    var finishedAt: String
+    var playersData: [HistoryPlayerData]
+}
+
+struct HistoryPlayerData: Hashable, Codable {
+    var name: String
+    var mark: Int
+    var score: Int
+}
